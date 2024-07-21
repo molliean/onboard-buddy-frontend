c1855a98508e401dd3e93ec4fe2dc25089ba9130
molliean <molliean29@gmail.com>
2024-07-20 21:33:39 -0700

    conditional rendering for delete button

--- src/components/Boards/BoardDetails.jsx
+++ src/components/Boards/BoardDetails.jsx
@@ -1,13 +1,14 @@
 //src/components/Boards/BoardDetails.jsx
 
 import { useParams, Link } from "react-router-dom"
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import * as boardService from '../../services/boardService'
 import { useLoggedUser } from "../../Contexts/UserContext";
+import styles from './BoardDetails.module.css'
 
-export default function BoardDetails ({handleDeleteBoard}) {
+export default function BoardDetails({ handleDeleteBoard }) {
     const loggedUser = useLoggedUser();
-    const {boardId} = useParams();
+    const { boardId } = useParams();
     // console.log(boardId, '<--board id')
     const [board, setBoard] = useState(null);
 
@@ -31,17 +32,30 @@ export default function BoardDetails ({handleDeleteBoard}) {
 
     return (
         <>
-        <h2>{board.boardName}</h2>
-        <h3>Tasks:</h3>
-        <ul>
-            {board.tasks.map((task) => {
-                return <li key={task._id}>{task.taskName}</li>
-            })}
-        </ul>
-        <button> <Link to={`/boards/${boardId}/tasks/new`}> Add Tasks</Link></button>
-        <button>Edit Board</button>
-        {/* {if loggedUser._id === board.owner._id} */}
-        <button onClick={() => handleDeleteBoard(boardId)}>Delete Board</button>
+            <div className={styles.boardDetails}>
+                <h2>{board.boardName}</h2>
+                <h3>Tasks:</h3>
+                <div>
+                {board.tasks.map((task) => {
+                    return (
+                        <div key={task._id} className={styles.taskCard}>
+                            <div className={styles.taskTitle}>{task.taskName}</div>
+                            <p>{task.description}</p>
+                        </div>
+                    );
+                })}
+            </div>
+                {/* <ul>
+                    {board.tasks.map((task) => {
+                        return <li key={task._id}>{task.taskName}</li>
+                    })}
+                </ul> */}
+                <button> <Link to={`/boards/${boardId}/tasks/new`}> Add Tasks</Link></button>
+                <button>Edit Board</button>
+                {loggedUser._id === board.owner && (
+                    <button onClick={() => handleDeleteBoard(boardId)}>Delete Board</button>
+                )}
+            </div>
         </>
     )
 }
